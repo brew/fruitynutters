f4572142c1fae9c76be7cd299437645870cd858a
Brook Elgie <specialbrew@gmail.com>
2008-09-22 11:27:25 +0000

    Django Aisle templates.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@11 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- webapps/fruitynutters/catalogue/views.py
+++ webapps/fruitynutters/catalogue/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+
+from fruitynutters.catalogue.models import Aisle
+
+def aisle(request, aisle_id):
+    """Aisle view"""
+    return render_to_response('aisle.html', locals())
+    
+def aisle_index(request):
+    aisle_list = Aisle.objects.all()
+    return render_to_response('aisle_index.html', locals())
--- webapps/fruitynutters/urls.py
+++ webapps/fruitynutters/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +15,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^catalogue/admin/(.*)', admin.site.root),
+)
+
+urlpatterns += patterns('fruitynutters.catalogue.views',
+    (r'^catalogue/aisle/$', 'aisle_index'),
+    (r'^catalogue/aisle/(?P<aisle_id>\d+)/$', 'aisle'),
 )
