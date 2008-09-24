bdd17c1a5c5b07484d3cb3f543b9cdaf487b97cd
Brook Elgie <specialbrew@gmail.com>
2008-09-24 16:39:57 +0000

    Empty cart javascript.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@21 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- www/javascript/fn_cart.js
+++ www/javascript/fn_cart.js
@@ -1,13 +1,14 @@
 var FNCart = Class.create({
   initialize: function() {
     // Intercept the product add form submit event.
+    
+    this.boundAddSubmit = this.onAddFormSubmit.bind(this);
     $$('.product_add').each(function(form, index) {
-      form.observe('submit', this.onAddFormSubmit.bind(this));
+      form.observe('submit', this.boundAddSubmit);
     }, this);
+
     
-    $('empty_list').observe('click', this.onEmptyList.bind(this));
-    
-    
+    this._prepareCart();
   },
   
   /**
@@ -20,15 +21,37 @@ var FNCart = Class.create({
   
   
   onEmptyList: function(ev) {
-    
+    this.updateCart("/cart/empty/");
+    ev.stop();
   },
   
   /**
    *  Updates the cart with a given url.
    */
   updateCart: function(url) {
-    new Ajax.Updater({success:'cart_content'}, ev.target.action, {
-      method:'post'
+    
+    this._prepareForUpdate();
+    
+    new Ajax.Updater({success:'cart_content'}, url, {
+      method:'post',
+      onComplete: function() {
+        console.info('completed update');
+        this._prepareCart();
+      }.bind(this)
     });    
+  },
+  
+  /**
+   *  Prepares cart for update by removing listeners.
+   */
+  _prepareForUpdate: function() {
+    this.boundEmptyList =this.onEmptyList.bind(this);
+    $('empty_list').stopObserving('click', this.boundEmptyList);
+  },
+  
+  _prepareCart: function() {
+    this.boundEmptyList =this.onEmptyList.bind(this);
+    $('empty_list').observe('click', this.boundEmptyList);
   }
+  
 });
