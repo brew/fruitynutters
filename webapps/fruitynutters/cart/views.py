e04dc95efe57aae07a7f35a984daf8c31e531442
Brook Elgie <specialbrew@gmail.com>
2008-09-24 17:24:19 +0000

    Activity indicator on cart.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@23 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- webapps/fruitynutters/cart/views.py
+++ webapps/fruitynutters/cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.sessions.models import Session
+from django.http import HttpResponseForbidden
 
 from fruitynutters.catalogue.models import Item
 from fruitynutters.cart.models import Cart, CartItem
@@ -12,12 +13,16 @@ def add_to_cart(request, item_id):
         cart.add_item(chosen_item=item_to_add, number_added=1)
         
         return render_to_response('cart.html', {'cart':cart, 'cart_items':cart.cartitem_set.all()})
+    
+    return HttpResponseForbidden()
         
 def empty_cart(request):
     if request.method == "POST":
         cart = _get_cart_by_id(request.session.get('cart_id'))
         cart.empty()
         return render_to_response('cart.html', {'cart':cart, 'cart_items':cart.cartitem_set.all()})
+        
+    return HttpResponseForbidden()
         
 def _get_cart_by_id(id):
     return Cart.objects.get(id__exact=id)
