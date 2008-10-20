58d259a12d8555b69893399c9d490fb12dfd45b7
Brook Elgie <specialbrew@gmail.com>
2008-10-20 17:43:27 +0000

    Basic orderform.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@67 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- webapps/fruitynutters/cart/views.py
+++ webapps/fruitynutters/cart/views.py
@@ -87,4 +87,125 @@ def submit(request):
     # document_renderer = Renderer()
     # document_renderer.Write(createOrderForm(cart, request.POST), response)
     
+<<<<<<< .mine
     return response
+    
+def createOrderForm(cart, member_details):
+    """Creates and returns an order form pdf."""
+    ss = makeReportStylesheet()
+    doc = Document(ss)
+    section = Section()
+    doc.Sections.append( section )
+
+    table = Table(  TabPS.DEFAULT_WIDTH,
+                    TabPS.DEFAULT_WIDTH * 8,
+                    TabPS.DEFAULT_WIDTH * 2,
+                    TabPS.DEFAULT_WIDTH * 2 )
+
+    thin_edge  = BorderPS( width=10, style=BorderPS.SINGLE )
+    thin_frame  = FramePS( thin_edge,  thin_edge,  thin_edge,  thin_edge )
+
+    # header
+    header_props = ParagraphPS(alignment=3)
+
+    c1_para = Paragraph(ss.ParagraphStyles.Heading2Short, header_props)
+    c1_para.append('O')
+    c1 = Cell(c1_para, thin_frame)
+    
+    c2_para = Paragraph(header_props)
+    c2_para.append('Product ordered')
+    c2 = Cell(c2_para, thin_frame )
+
+    c3_para = Paragraph(header_props)
+    c3_para.append('Amount')
+    c3 = Cell(c3_para, thin_frame )
+    
+    c4_para = Paragraph(header_props)
+    c4_para.append('Cost')
+    c4 = Cell(c4_para, thin_frame )
+    table.AddRow(c1, c2, c3, c4)
+
+    # list
+    for cart_item in cart.cartitem_set.all():
+        organic = ""
+        if cart_item.product.organic:
+            organic = "o"
+        
+        centre_props = ParagraphPS(alignment=3)    
+        
+        c1_para = Paragraph(ss.ParagraphStyles.Normal, centre_props)
+        c1_para.append(organic)
+        c1 = Cell(c1_para, thin_frame)
+        
+        c2_para = Paragraph()
+        c2_para.append(str(cart_item.product.name) + " (" + str(cart_item.product.unit_number) + " x " + str(cart_item.product.measure_per_unit) + str(cart_item.product.measure_type) + ")")
+        c2 = Cell(c2_para, thin_frame)
+        
+        c3_para = Paragraph(centre_props)
+        c3_para.append(str(cart_item.quantity))
+        c3 = Cell(c3_para, thin_frame)
+        
+        cost_props = ParagraphPS(alignment=2)
+        
+        c4_para = Paragraph(cost_props)
+        c4_para.append(str(cart_item.line_total))
+        c4 = Cell(c4_para, thin_frame)
+        table.AddRow(c1, c2, c3, c4)
+
+
+    c1_para = Paragraph(ss.ParagraphStyles.Heading2Short, ParagraphPS(alignment=2))
+    c1_para.append('Total cost')
+    c1 = Cell(c1_para, thin_frame, span=3)
+    
+    c2_para = Paragraph(ParagraphPS(alignment=2))
+    c2_para.append("\xa3"+str(cart.total))
+    c2 = Cell(c2_para,thin_frame)
+    table.AddRow(c1,c2)
+
+    section.append( table )
+
+    return doc
+    
+def makeReportStylesheet():
+	result = StyleSheet()
+
+	NormalText = TextStyle( TextPropertySet( result.Fonts.Arial, 20 ) )
+
+	ps = ParagraphStyle( 'Normal',
+						 NormalText.Copy(),
+						 ParagraphPropertySet( space_before = 60,
+											   space_after  = 60 ) )
+	result.ParagraphStyles.append( ps )
+
+	ps = ParagraphStyle( 'Normal Short',
+						 NormalText.Copy() )
+	result.ParagraphStyles.append( ps )
+
+	NormalText.TextPropertySet.SetSize( 32 )
+	ps = ParagraphStyle( 'Heading 1',
+						 NormalText.Copy(),
+						 ParagraphPropertySet( space_before = 240,
+											   space_after  = 60 ) )
+	result.ParagraphStyles.append( ps )
+	
+	NormalText.TextPropertySet.SetSize( 24 ).SetBold( True )
+	ps = ParagraphStyle( 'Heading 2',
+						 NormalText.Copy(),
+						 ParagraphPropertySet( space_before = 240,
+											   space_after  = 60 ) )
+	result.ParagraphStyles.append( ps )
+	
+	NormalText.TextPropertySet.SetSize( 24 ).SetBold( True )
+	ps = ParagraphStyle( 'Heading 2 Short',
+						 NormalText.Copy() )
+	result.ParagraphStyles.append( ps )
+
+	return result
+    
+    
+    
+    
+    
+    
+=======
+    return response>>>>>>> .r66
