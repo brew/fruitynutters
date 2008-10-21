5d2043b7a4538f12a0f644dc6bd9f711c2355cfe
Brook Elgie <specialbrew@gmail.com>
2008-10-21 16:31:06 +0000

    Tweeks to order form.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@70 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- webapps/fruitynutters/cart/order_form.py
+++ webapps/fruitynutters/cart/order_form.py
@@ -2,7 +2,7 @@
 
 from rtfng.Elements import StyleSheet, Document
 from rtfng.Styles import TextStyle, ParagraphStyle
-from rtfng.PropertySets import TextPropertySet, ParagraphPropertySet, TabPropertySet, BorderPropertySet, FramePropertySet
+from rtfng.PropertySets import TextPropertySet, ParagraphPropertySet, TabPropertySet, BorderPropertySet, FramePropertySet, MarginsPropertySet
 from rtfng.document.section import Section
 from rtfng.document.paragraph import Paragraph, Table, Cell
 
@@ -12,27 +12,32 @@ def createOrderForm(cart, member_details):
     member_name = member_details.get('member_name')
     member_email = member_details.get('member_email')
     member_phone = member_details.get('member_phone')
+    order_comments = member_details.get('order_comments')
 
     ss = makeReportStylesheet()
     doc = Document(ss)
-    section = Section()
+    section = Section(margins=MarginsPropertySet( top=600, left=600, bottom=600, right=600 ))
     doc.Sections.append( section )
 
-    footer_text = "Order for: " + member_name + ", phone: " + member_phone
-    section.Footer.append(str(footer_text))
+    header_text = Paragraph(ss.ParagraphStyles.Heading2Short, ParagraphPropertySet(alignment=3))
+    header_text.append(u"Fruity Nutters - Order form")
+    section.Header.append(header_text)
 
-    table = Table(  TabPropertySet.DEFAULT_WIDTH,
-                    TabPropertySet.DEFAULT_WIDTH * 8,
-                    TabPropertySet.DEFAULT_WIDTH * 2,
-                    TabPropertySet.DEFAULT_WIDTH * 2 )
+    footer_text = member_name + " -  Phone: " + member_phone
+    section.Footer.append(str(footer_text))
 
     thin_edge  = BorderPropertySet( width=10, style=BorderPropertySet.SINGLE )
     thin_frame  = FramePropertySet( thin_edge,  thin_edge,  thin_edge,  thin_edge )
 
+    table = Table(  TabPropertySet.DEFAULT_WIDTH,
+                    TabPropertySet.DEFAULT_WIDTH * 9,
+                    TabPropertySet.DEFAULT_WIDTH * 2,
+                    TabPropertySet.DEFAULT_WIDTH * 2 )
+
     # header
     header_props = ParagraphPropertySet(alignment=3)
 
-    c1_para = Paragraph(ss.ParagraphStyles.Heading2, header_props)
+    c1_para = Paragraph(ss.ParagraphStyles.Heading2Short, header_props)
     c1_para.append('O')
     c1 = Cell(c1_para, thin_frame)
 
@@ -64,7 +69,16 @@ def createOrderForm(cart, member_details):
         measure_per_unit = "%g" % cart_item.product.measure_per_unit
 
         c2_para = Paragraph()
-        c2_para.append(unicode(cart_item.product.name) + " (" + unicode(cart_item.product.unit_number) + " x " + str(measure_per_unit) + unicode(cart_item.product.measure_type) + ")")
+        c2_para.append(unicode(cart_item.product.name))
+        brand = cart_item.product.brand
+        if brand:
+            c2_para.append(" - " + unicode(brand))
+        c2_para.append(" (" + unicode(cart_item.product.unit_number) + " x " + str(measure_per_unit) + unicode(cart_item.product.measure_type) + ")")
+        if cart_item.product.bundle:
+            c2_para.append(u"\n")
+            for bundle_item in cart_item.cart_bundle.cartitem_set.all():
+                c2_para.append(unicode(bundle_item.product.name))
+                c2_para.append(u" x " + unicode(bundle_item.quantity) + u", ")
         c2 = Cell(c2_para, thin_frame)
 
         c3_para = Paragraph(centre_props)
@@ -78,9 +92,9 @@ def createOrderForm(cart, member_details):
         c4 = Cell(c4_para, thin_frame)
         table.AddRow(c1, c2, c3, c4)
 
-
-    c1_para = Paragraph(ss.ParagraphStyles.Heading2, ParagraphPropertySet(alignment=2))
-    c1_para.append('Total cost')
+    # table footer
+    c1_para = Paragraph(ss.ParagraphStyles.Heading2Short, ParagraphPropertySet(alignment=2))
+    c1_para.append(u'Total cost')
     c1 = Cell(c1_para, thin_frame, span=3)
 
     c2_para = Paragraph(ParagraphPropertySet(alignment=2))
@@ -89,6 +103,47 @@ def createOrderForm(cart, member_details):
     table.AddRow(c1,c2)
 
     section.append( table )
+    
+    section.append(Paragraph())
+
+    # Comments / Wishlist
+    comments_header = Paragraph(ss.ParagraphStyles.Heading2Short)
+    comments_header.append(u'Comments / Wishlist')
+    section.append(comments_header)
+    
+    table = Table(10000)
+    
+    comments_para = Paragraph(ss.ParagraphStyles.Normal)
+    comments_para.append( order_comments )
+    c1 = Cell(comments_para, thin_frame)
+    table.AddRow(c1)
+    
+    section.append(table)
+    
+    section.append(Paragraph())    
+    
+    # admin table
+    table = Table(  TabPropertySet.DEFAULT_WIDTH * 7,
+                    TabPropertySet.DEFAULT_WIDTH * 7)
+                    
+    c1_para = Paragraph(ss.ParagraphStyles.AdminPara)
+    c1_para.append(u'Extras')
+    c1 = Cell(c1_para, thin_frame)
+
+    c2_para = Paragraph()
+    c2_para.append(u'Refunds')
+    c2 = Cell(c2_para, thin_frame )    
+    table.AddRow(c1, c2)
+    
+    c1_para = Paragraph(ss.ParagraphStyles.AdminPara)
+    c1 = Cell(c1_para, thin_frame)
+
+    c2_para = Paragraph()
+    c2_para.append(u'Paid')
+    c2 = Cell(c2_para, thin_frame )    
+    table.AddRow(c1, c2)
+    
+    section.append(table)
 
     return doc
 
@@ -121,6 +176,21 @@ def makeReportStylesheet():
                          ParagraphPropertySet( space_before = 240,
                                                space_after  = 60 ) )
     result.ParagraphStyles.append( ps )
+    
+    NormalText.textProps.size = 24
+    NormalText.textProps.bold = True
+    ps = ParagraphStyle( 'Heading 2 Short',
+                         NormalText.Copy() )
+
+    result.ParagraphStyles.append( ps )
+    
+    result.ParagraphStyles.append(ParagraphStyle('Admin Para',
+                                NormalText.Copy(),
+                                ParagraphPropertySet(space_after = 2000)) )
+                                
+    result.ParagraphStyles.append(ParagraphStyle('Comments',
+                                NormalText.Copy(),
+                                ParagraphPropertySet(space_after = 200, space_before=200, left_indent=200, right_indent=200)) )
 
     return result
         
