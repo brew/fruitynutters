04e54658d83684d2a0d997999faba874a4ccf34b
Brook Elgie <specialbrew@gmail.com>
2008-11-09 12:53:32 +0000

    Fixed reset items bug.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@99 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- www/javascript/fn_admin_reset_items.js
+++ www/javascript/fn_admin_reset_items.js
@@ -1,11 +1,14 @@
 function initResetButton() {
   var resetButton = $('reset_items_button');
-  resetButton.observe('click', function() {
-    if (!window.confirm('Are you sure you want to reset all items in the catalogue?')) {
-      ev.stop();
-    }    
-  });
+  resetButton.observe('click', resetItems.bind(this));
 }
+
+function resetItems(ev) {
+  if (!window.confirm('Are you sure you want to reset all items in the catalogue?')) {
+    ev.stop();
+  }
+}
+
 
 // Set up reset button!
 Event.observe(window, 'load', function(event){
