d856464d511380156f9d86f062014c46d2285164
Brook Elgie <specialbrew@gmail.com>
2008-11-10 14:07:01 +0000

    Fiddling with WYM editor.
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@114 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- www/javascript/wymeditor/jquery.wymeditor.js
+++ www/javascript/wymeditor/jquery.wymeditor.js
@@ -386,10 +386,10 @@ jQuery.fn.wymeditor = function(options) {
     toolsItems: [
         {'name': 'Bold', 'title': 'Strong', 'css': 'wym_tools_strong'}, 
         {'name': 'Italic', 'title': 'Emphasis', 'css': 'wym_tools_emphasis'},
-        {'name': 'Superscript', 'title': 'Superscript',
-            'css': 'wym_tools_superscript'},
-        {'name': 'Subscript', 'title': 'Subscript',
-            'css': 'wym_tools_subscript'},
+        // {'name': 'Superscript', 'title': 'Superscript',
+        //     'css': 'wym_tools_superscript'},
+        // {'name': 'Subscript', 'title': 'Subscript',
+        //     'css': 'wym_tools_subscript'},
         {'name': 'InsertOrderedList', 'title': 'Ordered_List',
             'css': 'wym_tools_ordered_list'},
         {'name': 'InsertUnorderedList', 'title': 'Unordered_List',
@@ -400,10 +400,10 @@ jQuery.fn.wymeditor = function(options) {
         {'name': 'Redo', 'title': 'Redo', 'css': 'wym_tools_redo'},
         {'name': 'CreateLink', 'title': 'Link', 'css': 'wym_tools_link'},
         {'name': 'Unlink', 'title': 'Unlink', 'css': 'wym_tools_unlink'},
-        {'name': 'InsertImage', 'title': 'Image', 'css': 'wym_tools_image'},
-        {'name': 'InsertTable', 'title': 'Table', 'css': 'wym_tools_table'},
-        {'name': 'Paste', 'title': 'Paste_From_Word',
-            'css': 'wym_tools_paste'},
+        // {'name': 'InsertImage', 'title': 'Image', 'css': 'wym_tools_image'},
+        // {'name': 'InsertTable', 'title': 'Table', 'css': 'wym_tools_table'},
+        // {'name': 'Paste', 'title': 'Paste_From_Word',
+        //     'css': 'wym_tools_paste'},
         {'name': 'ToggleHtml', 'title': 'HTML', 'css': 'wym_tools_html'},
         {'name': 'Preview', 'title': 'Preview', 'css': 'wym_tools_preview'}
     ],
@@ -426,7 +426,7 @@ jQuery.fn.wymeditor = function(options) {
                         
     containersItems: [
         {'name': 'P', 'title': 'Paragraph', 'css': 'wym_containers_p'},
-        {'name': 'H1', 'title': 'Heading_1', 'css': 'wym_containers_h1'},
+        // {'name': 'H1', 'title': 'Heading_1', 'css': 'wym_containers_h1'},
         {'name': 'H2', 'title': 'Heading_2', 'css': 'wym_containers_h2'},
         {'name': 'H3', 'title': 'Heading_3', 'css': 'wym_containers_h3'},
         {'name': 'H4', 'title': 'Heading_4', 'css': 'wym_containers_h4'},
@@ -434,8 +434,8 @@ jQuery.fn.wymeditor = function(options) {
         {'name': 'H6', 'title': 'Heading_6', 'css': 'wym_containers_h6'},
         {'name': 'PRE', 'title': 'Preformatted', 'css': 'wym_containers_pre'},
         {'name': 'BLOCKQUOTE', 'title': 'Blockquote',
-            'css': 'wym_containers_blockquote'},
-        {'name': 'TH', 'title': 'Table_Header', 'css': 'wym_containers_th'}
+            'css': 'wym_containers_blockquote'}
+        // {'name': 'TH', 'title': 'Table_Header', 'css': 'wym_containers_th'}
     ],
 
     classesHtml:       "<div class='wym_classes wym_section'>"
