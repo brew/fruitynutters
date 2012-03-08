8de8040e058ffc5b8d7f8991fb4f692dd88b6f6c
Brook Elgie <specialbrew@gmail.com>
2012-03-08 10:37:11 +0000

    Add more picking order values
    git-svn-id: http://svn.lowest-common-denominator.com/fruitynutters@198 ae8131d6-8856-0410-bc7a-a2090b84ebb2

--- webapps/fruitynutters/catalogue/models.py
+++ webapps/fruitynutters/catalogue/models.py
@@ -92,6 +92,17 @@ class Item(models.Model):
         (7, '7'),
         (8, '8'),
         (9, '9'),
+        (10, '10'),
+        (11, '11'),
+        (12, '12'),
+        (13, '13'),
+        (14, '14'),
+        (15, '15'),
+        (16, '16'),
+        (17, '17'),
+        (18, '18'),
+        (19, '19'),
+        (20, '20'),
     )
     picking_order = models.IntegerField(choices=picking_order_choices, default=9, verbose_name='Picking Order')
 
