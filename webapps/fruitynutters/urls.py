c28fc42d0df64027d1f1c6b21a9e3b2f6af1b8d3
Brook Elgie <specialbrew@gmail.com>
2016-04-01 13:18:28 +0100

    Configure urls.py to serve media files

--- webapps/fruitynutters/urls.py
+++ webapps/fruitynutters/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import patterns, url, include
 
 # Uncomment the next two lines to enable the admin:
@@ -48,3 +50,5 @@ urlpatterns += patterns('fruitynutters.cart.views',
 urlpatterns += patterns('fruitynutters.catalogue.views',
     (r'^aislemock.html', 'aisle_mock'),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
