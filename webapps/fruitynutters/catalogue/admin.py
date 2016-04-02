95b375a86df7ed5f3834c18438403167c9babf05
Brook Elgie <specialbrew@gmail.com>
2016-04-02 17:26:34 +0100

    Prepopulate order_name property in items, instead of sort_name

--- webapps/fruitynutters/catalogue/admin.py
+++ webapps/fruitynutters/catalogue/admin.py
@@ -61,7 +61,7 @@ class ItemAdmin(admin.ModelAdmin):
     )
     search_fields = ['name', 'id']
     list_filter = ['aisle']
-    prepopulated_fields = {'sort_name': ('name',)}
+    prepopulated_fields = {'order_name': ('name',)}
     save_as = True
 
 admin.site.register(Item, ItemAdmin)
